9976e59558a1fca180c2b2f910985d426e3c9e9d
Rohit Thapar <thaprt206@gmail.com>
2023-01-22 15:02:04 +0530

    Changes

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 
 from distutils.core import setup
 setup(
-  name = '102003482-topsis-RohitThapar',         # How you named your package folder (MyLib)
-  packages = ['102003482-topsis-RohitThapar'],   # Chose the same as "name"
+  name = '1opsis-rohitThapar-102003482',         # How you named your package folder (MyLib)
+  packages = ['opsis-rohitThapar-102003482'],   # Chose the same as "name"
   version = '0.0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
   author = 'Rohit Thapar',                   # Type in your name
   author_email = 'thaprt206@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/rohitthapar/102003482-topsis-RohitThapar',   # Provide either the link to your github or to your website
+  url = 'https://github.com/rohitthapar/topsis-rohitThapar-102003482',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
